7d182e23cb3829d75e2e2d15911e0857c3d87589
Oguzhan Ergin <oguzhanergin13@gmail.com>
2019-06-02 23:20:50 +0100

    impl. boxkeypair generator

--- common/boxkeypair.go
+++ common/boxkeypair.go
@@ -1,6 +1,26 @@
 package common
 
+import (
+	"crypto/rand"
+
+	"golang.org/x/crypto/nacl/box"
+)
+
 type BoxKeyPair struct {
 	Pk [KeyBytesSize]byte
 	Sk [KeyBytesSize]byte
+}
+
+func NewBoxKeyPair(pk [KeyBytesSize]byte, sk [KeyBytesSize]byte) *BoxKeyPair {
+	return &BoxKeyPair{
+		Pk: pk,
+		Sk: sk,
+	}
+}
+func GenerateBoxKeyPair() (*BoxKeyPair, error) {
+	pk, sk, err := box.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	return NewBoxKeyPair(*pk, *sk), nil
 }
