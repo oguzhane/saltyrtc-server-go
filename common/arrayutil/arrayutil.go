dc8a2fb855556cc7c00f1ac4f6d212a586487f9d
Oguzhan Ergin <oguzhanergin13@gmail.com>
2019-08-11 22:45:31 +0100

    added arrayutil

---
+++ common/arrayutil/arrayutil.go
@@ -0,0 +1,16 @@
+package arrayutil
+
+func IntersectionStr(a, b []string) (c []string) {
+	m := make(map[string]bool)
+
+	for _, item := range a {
+		m[item] = true
+	}
+
+	for _, item := range b {
+		if _, ok := m[item]; ok {
+			c = append(c, item)
+		}
+	}
+	return
+}
