67c30767da713581083b21e06b9eb6bb2082ea4c
Oguzhan Ergin <oguzhanergin13@gmail.com>
2020-02-10 00:47:50 +0000

    cmd flags

--- cmd/saltyrtc-server-go/main.go
+++ cmd/saltyrtc-server-go/main.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/OguzhanE/saltyrtc-server-go/core"
-
-	_ "net/http/pprof"
+	"github.com/OguzhanE/saltyrtc-server-go/pkg/boxkeypair"
+	"github.com/OguzhanE/saltyrtc-server-go/pkg/hexutil"
 )
 
 var server *core.Server
 
 func main() {
-	core.InitLogger()
-	addr := ":3838"
 
-	server = core.NewServer()
+	var flags struct {
+		Addr      string
+		Port      uint
+		Verbosity int
+		Pk        string
+		Sk        string
+	}
+
+	flag.StringVar(&flags.Addr, "a", "", "Address")
+	flag.UintVar(&flags.Port, "p", 3838, "Port")
+	flag.IntVar(&flags.Verbosity, "v", 10, "Logging Verbosity")
+	flag.StringVar(&flags.Pk, "pk", "", "Public key of server permanent key in hex format")
+	flag.StringVar(&flags.Sk, "sk", "", "Secret key of server permanent key in hex format")
+	flag.Parse()
+
+	if flags.Sk == "" || flags.Pk == "" {
+		flag.Usage()
+		return
+	}
+
+	pkBytes, errPk := hexutil.HexStringToBytes32(flags.Pk)
+	skBytes, errSk := hexutil.HexStringToBytes32(flags.Sk)
+
+	if errPk != nil || errSk != nil {
+		log.Fatal("Invalid permanent key")
+		return
+	}
+
+	core.InitLogger(flags.Verbosity)
+
+	addr := fmt.Sprintf("%s:%d", flags.Addr, flags.Port)
+
+	defaultBox := boxkeypair.NewBoxKeyPair(*pkBytes, *skBytes)
+	core.Sugar.Info("Starting server with the public permanent key: ", flags.Pk)
+
+	server = core.NewServer(*defaultBox)
 	server.Start(addr)
 	quit := make(chan interface{})
 	select {
